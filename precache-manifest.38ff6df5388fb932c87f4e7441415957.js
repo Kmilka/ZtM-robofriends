88ff0189db1fd71979e948d29aa8ec95ea18ec8b
Kmilka <do.kseniya@gmail.com>
2020-07-22 22:18:40 +0300

    Updates

---
+++ precache-manifest.38ff6df5388fb932c87f4e7441415957.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1063514dd193d47d53e05b4397881b27",
+    "url": "/ztm-robofriends/index.html"
+  },
+  {
+    "revision": "432f108a009832a7ed8e",
+    "url": "/ztm-robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "a7ab455909ab243cd792",
+    "url": "/ztm-robofriends/static/css/main.2292e34c.chunk.css"
+  },
+  {
+    "revision": "432f108a009832a7ed8e",
+    "url": "/ztm-robofriends/static/js/2.6e6d0b07.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/ztm-robofriends/static/js/2.6e6d0b07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7ab455909ab243cd792",
+    "url": "/ztm-robofriends/static/js/main.4e417100.chunk.js"
+  },
+  {
+    "revision": "fc644a321fb87cb9c413",
+    "url": "/ztm-robofriends/static/js/runtime-main.597edd97.js"
+  }
+]);
